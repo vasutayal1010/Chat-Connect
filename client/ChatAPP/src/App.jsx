7c33937c3aacaafd30ab2a1de4e784986bd46207
Vasu Tayal <vasutayal1010@gmail.com>
2024-04-27 14:12:41 +0530

    Login/SignUp page

--- client/ChatAPP/src/App.jsx
+++ client/ChatAPP/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+const Login = lazy(()=> import("./pages/Login"))
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Chat = lazy(() => import("./pages/Chat"));
 
 const App = () => {
   return (
-    <div>
-      <h1>React</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/chat/:chatid" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
